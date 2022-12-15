0c5bf5d52f461699ffebe6d767f9dfcd4e61e363
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-12-15 18:25:29 -0500

    initial commit

---
+++ brainstorm.js
@@ -0,0 +1,56 @@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+let leagueMembers = {
+  teamOne: 
+    {
+      pointsFor: 1648.94,
+      wins: 12,
+      losses: 3,
+      ties: 0,
+    },
+  teamTwo: 
+    {
+      pointsFor: 1804.00,
+      wins: 8,
+      losses: 6,
+      ties: 0,
+    },
+  teamThree: 
+    {
+      pointsFor: 1567.62,
+      wins: 5,
+      losses: 9,
+      ties: 0,
+    },
+  teamFour: 
+    {
+      pointsFor: 1622.72,
+      wins: 6,
+      losses: 8,
+      ties: 0,
+    },
+}
+
+// should always add up to 2 (if two games where played)
+// teamOne.teamTwo + teamTwo.teamOne === 2;
+let headToHead = {
+  teamOne: {
+    teamTwo: 1,  // Tie (if 2 games were played between teams)
+    teamThree: 2, // Team one wins tie breaker
+    teamFour: 0, // Team four wins tie breaker
+  },
+  teamTwo: {
+    teamOne: 1,
+    teamThree: 1,
+    teamFour: 0
+  },
+  teamThree: {
+    teamOne: 0, // should be the opposite of teamOne/teamThree
+    teamTwo: 1,
+    teamFour: 2
+  },
+  teamFour: {
+    teamOne: 2,
+    teamTwo: 2,
+    teamThree: 0
+  }
+}
