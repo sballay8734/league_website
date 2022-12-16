4e13c76dbc4366c81718be32cbfcbe009cf975b5
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-12-16 18:11:11 -0500

    brainstorm

---
+++ stat-functions.js
@@ -0,0 +1,29 @@
+// maybe
+const seasonalManagers = () => {
+  let listOfObjects = [
+    {managers2016: ["steve", "cody", "aaron", "donnie"]},
+    {managers2017: ["flipp", "shawn", "dom", "aaron"]}
+  ]
+  return listOfObjects;
+}
+
+const seasonsInLeague = (manager) => {
+  let totalSeasons = 0;
+  // logic to calculate totalSeasons
+}
+
+const yearlyWinPercentage = (manager, year) => {
+  // logic
+}
+
+const allTimeWinPercentage = (manager) => {
+  // logic
+}
+
+const yearlyAvgPointsPerWeek = (manager) => {
+  // logic
+}
+
+const allTimeAvgPointsPerWeek = (manager) => {
+  // logic
+}
